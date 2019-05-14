4ed34d43a262708e8c77801fec9476ec63877cae
张旭 <zhangxu@udesk.cn>
2019-05-14 23:58:52 +0800

    02

---
+++ src/config/menuConfig.jsx
@@ -0,0 +1,27 @@
+/*
+ * @Author: Zhangxu
+ * @Date: 2019-05-14 23:17:58
+ * @LastEditors: Zhangxu
+ * @LastEditTime: 2019-05-14 23:46:00
+ */
+
+export default [{
+  title: '首页',
+  key: '/admin/home'
+}, {
+  title: 'UI',
+  key: '/admin/ui',
+  children: [{
+    title: '按钮',
+    key: '/admin/ui/buttons'
+  }, {
+    title: '弹窗',
+    key: '/admin/ui/model'
+  }, {
+    title: '',
+    key: ''
+  }, {
+    title: '',
+    key: ''
+  }]
+}]
